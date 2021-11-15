fa5370b84c045512e3c356ae24de0ac002c12442
elevey-d <83984831+elevey-d@users.noreply.github.com>
2021-11-16 00:54:03 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<footer>
+	<div id="footer">
+		<div class="container">
+			<div class="row row-bottom-padded-md">
+				<div class="col-md-8 col-sm-8 col-xs-12 fh5co-footer-link">
+					<h3>About Travel</h3>
+					<p>Travel is the movement of people between distant geographical locations. Travel can be done by foot, bicycle, automobile, train, boat, bus, airplane, ship or other means, with or without luggage, and can be one way or round trip. Travel can also include relatively short stays between successive movements, as in the case of tourism.</p>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-12 fh5co-footer-link">
+					<h3>Best Places</h3>
+					<ul>
+						<li><a href="#">Boracay Beach</a></li>
+						<li><a href="#">Dubai</a></li>
+						<li><a href="#">Singapore</a></li>
+						<li><a href="#">Hongkong</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<p class="fh5co-social-icons">
+						<a href="#"><i class="icon-twitter2"></i></a>
+						<a href="#"><i class="icon-facebook2"></i></a>
+						<a href="#"><i class="icon-instagram"></i></a>
+						<a href="#"><i class="icon-dribbble2"></i></a>
+						<a href="#"><i class="icon-youtube"></i></a>
+					</p>
+					<p>Projekt Języki Internetowe</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
